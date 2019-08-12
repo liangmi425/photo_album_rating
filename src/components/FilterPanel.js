bdde10ebed8a9b308c84a94ab7164dd01f3b3d3b
Aleksei <aleksei@fantasylab.io>
2019-08-12 22:45:30 +0800

    Updates

--- src/components/FilterPanel.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class extends Component {
-  render() {
-    return (
-      <a href="#" onClick={() => this.props.onClick}>{this.props.category}</a>
-    )
-  }
-}
