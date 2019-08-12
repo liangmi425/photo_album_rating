924790c9a4b1bdbbeb7781beb1fb401632c8825a
Aleksei <aleksei@fantasylab.io>
2019-08-12 22:43:23 +0800

    Updates

---
+++ precache-manifest.f9e2de55ccf08fa3b0ef0b6b917cc222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3f630633a45a27e075eeeec01f8368",
+    "url": "/photo-album-rating/index.html"
+  },
+  {
+    "revision": "05f288d7edd37d6bae75",
+    "url": "/photo-album-rating/static/css/2.7ccc5c51.chunk.css"
+  },
+  {
+    "revision": "46377063bc998d1f3f89",
+    "url": "/photo-album-rating/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "05f288d7edd37d6bae75",
+    "url": "/photo-album-rating/static/js/2.9aa9e2d7.chunk.js"
+  },
+  {
+    "revision": "46377063bc998d1f3f89",
+    "url": "/photo-album-rating/static/js/main.396b9c5a.chunk.js"
+  },
+  {
+    "revision": "d7ca14d1b5075992a70e",
+    "url": "/photo-album-rating/static/js/runtime~main.bc84f19f.js"
+  }
+]);
